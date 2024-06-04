253d774d21cf7d1283d0b26a755a3fe6cc1d7c16
mehdi <mhd.sabet63@gmail.com>
2024-06-04 18:44:46 +0330

    FEATURES:Intial BDD Sample Project.

---
+++ CancellationTokenSample/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interfaces;
+
+namespace CancellationTokenSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        private IApplicationDbContext _context;
+        public ProductController(IApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+
+        [HttpGet("Get-All-Products")]
+        public async Task<ActionResult> GetWithEFQueryCancelation()
+        {
+            var result = await _context.GetProductsAsync(HttpContext.RequestAborted);
+
+            return Ok(result);
+        }
+    }
+}
